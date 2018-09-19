0cd972a8641f842971bd4afb8f929dd0eda81370
ktrifonov <k.trifonov@zonatelecom.ru>
2018-09-19 18:13:01 +0300

    Part 2. Lesson 7 or 8

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var http = require('http');
 var path = require('path');
 var config = require('config');
 var log = require('lib/log')(module);
+var mongoose = require('lib/mongoose');
+var HttpError = require('error').HttpError;
 
 var app = express();
+
 app.engine('ejs', require('ejs-locals'))
 app.set('views', __dirname + '/template');
 app.set('view engine', 'ejs');
@@ -16,57 +19,91 @@ if (app.get('env') == 'development') {
   app.use(express.logger('default'));
 }
 app.use(express.bodyParser());
+
 app.use(express.cookieParser());
+
+var MongoStore = require('connect-mongo')(express);
+
+app.use(express.session({
+  secret: config.get('session:secret'),
+  key: config.get('session:key'),
+  cookie: config.get('session:cookie'),
+  store: new MongoStore({mongoose_connection: mongoose.connection})
+}));
+
+app.use(function(req, res, next) {
+  req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+  res.send("Visits: " + req.session.numberOfVisits);
+});
+
+app.use(require('middleware/sendHttpError'));
+
 app.use(app.router);
 
-app.get('/', function(req, res, next) {
-  res.render("index");
-});
+require('routes')(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next) {
-  if (app.get('env') == 'development') {
-    var errorHandler = express.errorHandler()
-    errorHandler(err, req, res, next);
+  if (typeof err == 'number') {
+    err = new HttpError(err);
+  }
+  if (err instanceof HttpError) {
+    res.sendHttpError(err);
   } else {
-    res.send(500);
+    if (app.get('env') == 'development') {
+      express.errorHandler()(err, req, res, next);
+    } else {
+      log.error(err);
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }
   }
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
 
-// Database Name
-const dbName = 'chat';
-const collectionName = "test_insert";
+// // Database Name
+// const dbName = 'chat';
+// const collectionName = "test_insert";
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
-  removeDocument(db, function() {
-    insertDocuments(db, function() {
-      findDocuments(db, function() {
-        client.close();
-      });
-    });
-  })
+//   const db = client.db(dbName);
+//   removeDocument(db, function() {
+//     insertDocuments(db, function() {
+//       findDocuments(db, function() {
+//         client.close();
+//       });
+//     });
+//   })
 
-});
+// });
 
-const removeDocument = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection(collectionName);
-  collection.deleteMany({}, function(err, res) {
-    callback(res);
-  })
-}
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test');
+
+// const schema = mongoose.Schema({
+//   name: String
+// });
+// schema.methods.meow = function() {
+//   console.log(this.get('name'));
+// }
+
+// const removeDocument = function(db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection(collectionName);
+//   collection.deleteMany({}, function(err, res) {
+//     callback(res);
+//   })
+// }
 
 
 const insertDocuments = function(db, callback) {
@@ -88,7 +125,8 @@ const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection(collectionName);
   // Find some documents
-  collection.find({}).toArray(function(err, docs) {
+  let cursor = collection.find({})
+  cursor.toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
     console.log(docs)
@@ -109,6 +147,55 @@ const findDocuments = function(db, callback) {
 // app.get('/', routes.index);
 // app.get('/users', user.list);
 
+
+
 http.createServer(app).listen(config.get('port'), function(){
   log.info("Express server listening on port " + config.get('port'));
 });
+
+// const mongoose = require('lib/mongoose');
+// mongoose.set('debug', true);
+// const async = require('async');
+
+
+// async.series([
+//   open,
+//   dropDatabase,
+//   requireModels,
+//   createUsers
+// ], function(err, result) {
+//   console.log(arguments);
+//   mongoose.disconnect();
+//   process.exit(err ? 255 : 0);
+// });
+
+function open(callback) {
+  mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+  const db = mongoose.connection.db;
+  db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+  require('models/user');
+  async.each(Object.keys(mongoose.models), function(modelName, callback) {
+    mongoose.models[modelName].ensureIndexes(callback);
+  }, callback);
+}
+
+function createUsers(callback) {
+  require('models/user');
+
+  var users = [
+    {username: 'Вася', password: 'supervasya'},
+    {username: 'Петя', password: '123'},
+    {username: 'admin', password: 'thetruehero'}
+  ];
+
+  async.each(users, function(userData, callback) {
+    var user = new mongoose.models.User(userData);
+    user.save(callback);
+  }, callback);
+}
--- createDb.js
+++ createDb.js
@@ -1,36 +1,104 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const User = require('models/user').User;
 
-// Connection URL
-const url = 'mongodb://localhost:27017/chat';
-
-// Database Name
-const dbName = 'test_insert';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  const collection = db.collection('documents');
-
-  collection.insert({a: 2}, insertDocuments);
-
-  const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-      {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-  }
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
+const user = new User({
+  username: "Tester",
+  password: "secret"
 });
+
+user.save(function(err, user, affected) {
+  if (err) throw err;
+  console.log(arguments);
+});
+
+// schema.methods.meow = function() {
+//   console.log(this.get('name'));
+// }
+
+// const Cat = mongoose.model('Cat', schema);
+
+// const kitty = new Cat({
+//   name: 'Zildjian',
+// });
+
+// kitty.save(function(err, product, affected) {
+//   product.meow();
+// });
+
+// // var User = require('models/user').User;
+
+// // var user = new User({
+// //   username: "Tester3",
+// //   password: "secret"
+// // });
+
+// // user.save(function(err, user, affected) {
+// //   console.log(1);
+// //   if (err) throw err;
+// //   console.log('OK');
+// //   User.findOne({username: "Tester3"}, function(err, tester) {
+// //     console.log(tester);
+// //   });
+// // });
+
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
+
+// // Database Name
+// const dbName = 'chat';
+// const collectionName = "test_insert";
+
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+
+//   const db = client.db(dbName);
+//   removeDocument(db, function() {
+//     insertDocuments(db, function() {
+//       findDocuments(db, function() {
+//         client.close();
+//       });
+//     });
+//   })
+
+// });
+
+// const removeDocument = function(db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection(collectionName);
+//   collection.deleteMany({}, function(err, res) {
+//     callback(res);
+//   })
+// }
+
+
+// const insertDocuments = function(db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection(collectionName);
+//   // Insert some documents
+//   collection.insertMany([
+//     {a : 1}, {a : 2}, {a : 3}
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     console.log("Inserted 3 documents into the collection");
+//     callback(result);
+//   });
+// }
+
+// const findDocuments = function(db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection(collectionName);
+//   // Find some documents
+//   let cursor = collection.find({})
+//   cursor.toArray(function(err, docs) {
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(docs)
+//     callback(docs);
+//   });
+// }
